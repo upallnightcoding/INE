4329f0868108a031095fbaa07420a9dfa82d9cc1
upallnightcoding <upallnightcoding@gmail.com>
2024-10-19 18:46:45 -0400

    Implemented D1 Animation

--- INE/Assets/20 - Characters/Player/PlayerCntrl.cs
+++ INE/Assets/20 - Characters/Player/PlayerCntrl.cs
@@ -6,42 +6,104 @@ using UnityEngine.InputSystem;
 
 public class PlayerCntrl : MonoBehaviour
 {
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject projectile01;
+
     private CharacterController charCntrl;
+    private Animator animator;
 
     private float playerSpeed = 5.0f;
+    private Vector3 direction;
 
     private Vector2 moveInput;
     private Vector3 moveDirection;
+    private Vector3 target;
 
     // Start is called before the first frame update
     void Start()
     {
-        charCntrl = GetComponent<CharacterController>();    
+        charCntrl = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        animator.SetFloat("speed", 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveCharacter();   
+        ClickAndMove(Time.deltaTime);
+        FireKey();
     }
 
-    private void MoveCharacter()
+    private void ClickAndMove(float dt)
     {
-        if (moveInput.magnitude > 0.3f)
-        {
-            moveDirection.x = moveInput.x;
-            moveDirection.y = 0.0f;
-            moveDirection.z = moveInput.y;
+        float throttle = 1.0f;
+        float speed = 5.0f;
 
-            charCntrl.Move(moveDirection * playerSpeed * Time.deltaTime);
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                target = new Vector3(hit.point.x, 0.0f, hit.point.z);
+
+                direction = (target - transform.position).normalized;
+
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                Quaternion playerRotation = targetRotation;
+                //Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, 25.0f * dt);
+                transform.localRotation = playerRotation;
+
+                transform.Translate(transform.forward * speed * throttle * dt, Space.World);
+
+                animator.SetFloat("speed", 1.3f);
+            }
+        } else
+        {
+            if (Vector3.Distance(transform.position, target) > 0.3f)
+            {
+                transform.Translate(transform.forward * speed * throttle * dt, Space.World);
+            } else
+            {
+                animator.SetFloat("speed", 0.0f);
+            }
         }
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+    private void FireKey()
     {
-        if (context.performed)
+        if (Keyboard.current.digit1Key.wasPressedThisFrame)
         {
-            moveInput = context.ReadValue<Vector2>();
+            Debug.Log("Fire 1 ... Start");
+            GameObject go = Instantiate(projectile01, firePoint.transform.position, Quaternion.identity);
+            go.GetComponentInChildren<Rigidbody>().AddForce(direction * 100.0f, ForceMode.Impulse);
+            Debug.DrawLine(firePoint.transform.position, firePoint.transform.position + direction * 10.0f, Color.red) ;
+            Destroy(go, 2.0f);
+        }
+
+        if (Keyboard.current.digit1Key.wasReleasedThisFrame)
+        {
+            Debug.Log("Fire 1 ... Canceled");
+        }
+
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            Debug.Log("Fire 2 ... Start");
+        }
+
+        if (Keyboard.current.digit2Key.wasReleasedThisFrame)
+        {
+            Debug.Log("Fire 2 ... Canceled");
+        }
+
+        if (Keyboard.current.digit3Key.wasPressedThisFrame)
+        {
+            Debug.Log("Fire 3 ... Start");
+        }
+
+        if (Keyboard.current.digit3Key.wasReleasedThisFrame)
+        {
+            Debug.Log("Fire 3 ... Canceled");
         }
     }
 }
